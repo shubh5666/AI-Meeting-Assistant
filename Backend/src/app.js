bc25dbdf392081c32a6d0ea60ef93a7d23a0d667
shubh56666 <154400060+shubh56666@users.noreply.github.com>
2026-06-12 11:02:39 +0530

    Render deployment setup

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -29,7 +29,9 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173"
+    ],
     credentials: true,
   })
 );
@@ -48,6 +50,8 @@ app.use(aiRouter);
 
 
 
-app.listen(process.env.PORT, () => {
-  console.log('Server is running on port 4000');
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
